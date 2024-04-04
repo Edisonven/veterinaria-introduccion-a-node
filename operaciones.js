389604a6e7ac860c8723611612cbad8ac5fdf102
Edison <aalevenegass@gmail.com>
2024-04-04 15:57:36 -0300

    usando fs de manera sincrona

--- operaciones.js
+++ operaciones.js
@@ -1,22 +1,14 @@
 const fs = require("node:fs");
 
 const registrar = (nombre, edad, tipo, color, enfermedad) => {
-  try {
-    const animales = JSON.parse(fs.readFileSync("./citas.json", "utf-8"));
-    animales.push({ nombre, edad, tipo, color, enfermedad });
-    fs.writeFileSync("./citas.json", JSON.stringify(animales));
-  } catch (error) {
-    console.error("Error al escribir en el archivo:", error);
-  }
+  const animales = JSON.parse(fs.readFileSync("./citas.json", "utf-8"));
+  animales.push({ nombre, edad, tipo, color, enfermedad });
+  fs.writeFileSync("./citas.json", JSON.stringify(animales));
 };
 
 const leer = () => {
-  try {
-    const leyendo = JSON.parse(fs.readFileSync("./citas.json", "utf-8"));
-    console.log(leyendo);
-  } catch (error) {
-    console.error("Error al leer el archivo:", error);
-  }
+  const leyendo = JSON.parse(fs.readFileSync("./citas.json", "utf-8"));
+  console.log(leyendo);
 };
 
 module.exports = { leer, registrar };
